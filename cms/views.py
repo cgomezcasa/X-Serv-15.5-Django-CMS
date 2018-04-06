057b18bd0e815dbbc626774b7c8099b1f3717244
cgomezcasa <cayegomez1995@gmail.com>
2018-04-06 17:08:01 +0200

    Práctica cms

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pages
 
 # Create your views here.
+
+def processRequest(request, recurso):
+    if request.method == 'GET':
+        try:
+            objeto = Pages.objects.get(id = int(recurso))
+            resp = objeto.page
+        except Pages.DoesNotExist:
+            resp = "objeto no contenido"
+        return HttpResponse(resp)
+
+def lista(request):
+    list = Pages.objects.all()
+    resp = "<ol>"
+    for objeto in list:
+        resp += '<li><a href="' + str(objeto.id) + '">' + objeto.name + '</a>'
+    resp += "</ol>"
+    return HttpResponse(resp)
+
+def notOption(request, recurso):
+    return HttpResponse("No contemplada esta opción.")
--- myproject/urls.py
+++ myproject/urls.py
@@ -7,4 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^cms/$', 'cms.views.lista'),
+    url(r'^cms/(\d+)', 'cms.views.processRequest'),   
+    url(r'^(.*)', 'cms.views.notOption'), 
 )
